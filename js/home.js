26d9f260cf33b172fdb4da2a4a304997a7a0560b
Minh Phương <dminhphuong97@gmail.com>
2025-10-15 10:17:38 +0700

    fix

--- js/home.js
+++ js/home.js
@@ -56,4 +56,35 @@ document.addEventListener('DOMContentLoaded', () => {
   });
 
 
+const track = document.querySelector('.hero_track');
+let isDown = false;
+let startX;
+let scrollLeft;
+
+track.addEventListener('mousedown', e => {
+  isDown = true;
+  track.classList.add('active');
+  startX = e.pageX - track.offsetLeft;
+  scrollLeft = track.scrollLeft;
+});
+
+track.addEventListener('mouseleave', () => {
+  isDown = false;
+  track.classList.remove('active');
+});
+
+track.addEventListener('mouseup', () => {
+  isDown = false;
+  track.classList.remove('active');
+});
+
+track.addEventListener('mousemove', e => {
+  if(!isDown) return;
+  e.preventDefault();
+  const x = e.pageX - track.offsetLeft;
+  const walk = (x - startX) * 2; // tốc độ kéo
+  track.scrollLeft = scrollLeft - walk;
+});
+
+
   
